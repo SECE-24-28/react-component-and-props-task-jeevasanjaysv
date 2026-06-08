0e349face6ce990760a4ff7c2b7e86771354001b
jeevasanjaysv <jeevasanjay.s2024ece@sece.ac.in>
2026-06-08 15:57:21 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Student from "./assets/components/Student"
+
+function App() {
+    return (
+      <>
+      <Student name="jeeva" age={18} dept="ECE"/>
+      </>
+    )
+
+}
+
+export default App
+  
---
+++ src/assets/components/Student.jsx
@@ -0,0 +1,7 @@
+export default function Student({name,dept,age}){
+    return( <>
+        <h1> {name}</h1>
+         <h1> {dept}</h1>
+          <h1> {age}</h1>
+    </>)
+}
